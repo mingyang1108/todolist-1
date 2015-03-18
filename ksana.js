27480bad8eec410ea0bb8b2655c717fde78615f5
mingyang <mingyang1108@gmail.com>
2015-03-18 16:28:12 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,45 @@
+jsonp_handler({
+ "version": "1",
+ "build": 36,
+ "title": "todolist-1",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/mingyang1108/todolist-1/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css"
+ ],
+ "filesizes": [
+  547,
+  115,
+  652507,
+  95357,
+  277980,
+  783,
+  35607,
+  113502
+ ],
+ "filedates": [
+  "2015-03-18T05:41:51.578Z",
+  "2015-03-18T08:22:18.265Z",
+  "2015-03-18T05:41:51.703Z",
+  "2015-03-18T08:24:40.333Z",
+  "2015-03-18T05:41:51.656Z",
+  "2015-03-18T08:24:12.527Z",
+  "2015-03-18T05:41:51.563Z",
+  "2015-03-18T05:41:51.547Z"
+ ],
+ "date": "2015-03-18T08:24:41.305Z"
+})
